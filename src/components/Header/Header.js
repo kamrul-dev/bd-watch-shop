f1639010e45da9d7d5635cb1f4bbce41194b2e3f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-25 22:47:05 +0600

    Header component completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,13 @@
 import './Header.css';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <h1>BD-Watch-Shop</h1>
+            <h2>Select 4 Watches </h2>
+        </nav>
+    );
+};
+
+export default Header;
