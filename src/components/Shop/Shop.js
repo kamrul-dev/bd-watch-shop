db215cbe55cf1fc4d3ab08bd9decbeb70a5545f0
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-25 22:58:55 +0600

    product section and cart section divided

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,1 +1,18 @@
-import './Shop.css'
+import './Shop.css';
+import React from 'react';
+
+// Shop component for loading thte data from public folder [data.json]
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div>
+                <h1>shop for product</h1>
+            </div>
+            <div>
+                <h2>Selected Watches</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
