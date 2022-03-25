ee4c5baa26e76bbaa71474f852a7b74e3a552708
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-25 23:05:28 +0600

    Data loaded inside the Shop component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,8 +1,15 @@
 import './Shop.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Shop component for loading thte data from public folder [data.json]
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    console.log(products);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
     return (
         <div className='shop-container'>
             <div>
