ada7afad9583c733c2e8f5fc28d57112376b6127
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-26 00:15:54 +0600

    Data display on UI and Styles added for product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,18 @@ import './Product.css';
 
 import React from 'react';
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { id, name, image, price } = product;
     return (
-        <div>
-            
+        <div className='product'>
+            <img src={image} alt="" />
+            <div className='product-info'>
+                <h2>{name}</h2>
+                <p>Price: ${price}</p>
+            </div>
+            <button className='cart-btn'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
